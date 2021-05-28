2bc328523a9672a1065635488c416542618eb949
fmanna <>
2021-05-28 08:59:43 +0000

    I saved my plot as a PNG file called "my_first_plot.png"

--- Exercise_7_problem_1.py
+++ Exercise_7_problem_1.py
@@ -72,7 +72,7 @@ plt.ylabel("Y-label")
 outputfp = "my_first_plot.png"
 
 # YOUR CODE HERE 5
-
+plt.savefig('my_first_plot.png')
 
 # This test print statement should print the output filename of your figure
 print("Saved my first plot as:", outputfp)
